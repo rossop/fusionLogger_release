32e7b8538a8aa16fb6e2de608dc63693d2ab5d96
rossop <peter.rosso@bristol.ac.uk>
2020-12-08 14:51:58 +0000

    Working Version

--- fusionLogger.py
+++ fusionLogger.py
@@ -1,173 +1,245 @@
-# Author- rossop
-# Description - 
+# Author- Peter Rosso  - rosso.io - Design and Manufacture Futures Lab
+# Description-Fusion Logger shows your learning history and recommends new commands to use.
+# LOG File by AdbA Icons ❤️ from the Noun Project
+# stop sign by Michael Finney from the Noun Project
 
-import adsk.core, adsk.fusion, adsk.cam, traceback
-import os
-import sys
-import logging
+import adsk, adsk.core, traceback, json, webbrowser, logging, os
 
-# Global variable used to maintain a reference to all event handlers.
-try:
-    app = adsk.core.Application.get()
-    ui  = app.userInterface
-    mouse = adsk.core.MouseEvent
-    camera = app.cameraChanged
+_app = adsk.core.Application.cast(None)
+_addin = None
+_ui = None
+_userId = None
+_userName = None
+_version = None
+_stopButton = None
 
-    # Global variable used to maintain a reference to all event handlers.
-    handlers = []
-    command_var = adsk.core.Command
+log = None
+logger_handler = None
+handlers = []
 
-    _userId = app.userId
-    _userName = app.userName
-    _version = app.version
+LOG_FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Fusion-logger-addin.log')
+PALETTE_ID = 'commandMapPalette'
+COMMAND_ID = 'StartFusionLogger'
+COMMAND_ID_STOP = 'StopFusionLogger'
+PALETTE_URL = 'https://commandmap.autodesk.com/v1/'
 
-except:
-    if ui:
-        ui.messageBox('Error:\n{}'.format(traceback.format_exc()))
+def bindEventHandler(event, handler):
+    event.add(handler)
+    handlers.append(handler)
 
-# Set up fusionLogger
-try:
-    filename = 'fusion360_' + _userName + '.log'
-    
-    fusion_logger = logging.getLogger(__name__)
-    fusion_logger.setLevel(logging.INFO)
-
-    LOG_FORMAT = '%(asctime)s|%(msecs)03d|%(process)d|%(filename)s|%(levelname)s|%(message)s'
-    formatter = logging.Formatter(LOG_FORMAT)
-    
-    if sys.platform == 'win32' or sys.platform == 'win64':
-        desktop = os.path.expanduser("~\Desktop")
-    elif sys.platform == 'darwin':
-        desktop = os.path.expanduser("~/Desktop")
-    PATH = os.path.join(desktop, filename)
+def unbindEventHandler(event, handler):
+    event.remove(handler)
+    handlers.remove(handler)
 
 
-    ui.messageBox(PATH)
-
-    file_handler = logging.FileHandler(PATH)
-    file_handler.setFormatter(formatter)
-
-    fusion_logger.addHandler(file_handler)
-    
-except:
-    if ui:
-        ui.messageBox('Error:\n{}'.format(traceback.format_exc()))
-
-# Global variable used to maintain a reference to all event handlers.
-try:
-    app = adsk.core.Application.get()
-    ui  = app.userInterface
-    mouse = adsk.core.MouseEvent
-    camera = app.cameraChanged
-
-    # Global variable used to maintain a reference to all event handlers.
-    handlers = []
-    command_var = adsk.core.Command
-
-    _userId = app.userId
-    _userName = app.userName
-    _version = app.version
-
-except:
-    if ui:
-        ui.messageBox('Error:\n{}'.format(traceback.format_exc()))
-
-
-# Event handler for the Comand start event
-class MyCommandStartingHandler(adsk.core.ApplicationCommandEventHandler):
+# # Event handler for the palette close event.
+class MyCloseEventHandler(adsk.core.UserInterfaceGeneralEventHandler):
     def __init__(self):
         super().__init__()
-        app =adsk.core.Application.get()
-        ui = app.userInterface
-    
-
     def notify(self, args):
-        eventArgs = adsk.core.ApplicationCommandEventArgs.cast(args)
+        pass
 
-        # Code to react to the event.
+
+# Event handler for the commandStarting event.
+class CommandStartingHandler(adsk.core.ApplicationCommandEventHandler):
+    def __init__(self):
+        super().__init__()
+    def notify(self, args):
+        ui = None
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+
         try:
-            objectType = str(eventArgs._get_objectType())
-            commandId = str(eventArgs._get_commandId())
-            editObj = str(app.activeEditObject.objectType)
-            document = str(app.activeDocument.name)
-            workspace = str(ui.activeWorkspace.name)
-            workspaceID = str(ui.activeWorkspace.id)
+            eventArgs = adsk.core.ApplicationCommandEventArgs.cast(args)
             
-            INFO = f'{_userId}|{_userName}|{_version}|{self.__class__}|{document}|{editObj}|w:{workspace}|{workspaceID}|{objectType}|{commandId}'
-            fusion_logger.info(INFO)
-            # obj_list = [method_name for method_name in dir(eventArgs)
-            #           if callable(getattr(eventArgs, method_name))]
-            # fusion_logger.info('obj list:{}'.format(str(obj_list)))
+            
+            objectType = eventArgs.objectType
+            commandId = eventArgs.commandId
+            editObj = app.activeEditObject.objectType
+            document = app.activeDocument.name
+            workspace = ui.activeWorkspace.name
+            workspaceID = ui.activeWorkspace.id
+            className = str(self.__class__).split("_py.")
+            className = className[-1]
 
+            if commandId:
+                INFO = f'{_userId}|{_userName}|{_version}|{className}|{document}|{editObj}|w:{workspace}|{workspaceID}|{objectType}|CommandStarting|{commandId}'
+                log.info(INFO)
+                
+            log.debug('Command started: {}'.format(commandId))
         except:
-            error_message = 'Logger failed:\n{}'.format(traceback.format_exc())
-            ui.messageBox(error_message)   
-
-
-class MyCameraChangedHandler(adsk.core.CameraEventHandler):
-    def __init__(self):
-        super().__init__()
-        app =adsk.core.Application.get()
-        ui = app.userInterface
-
-    def notify(self, args):
-        eventArgs = adsk.core.CameraEventArgs.cast(args)
-
-        try:
-            objectType = str(eventArgs._get_objectType())
-            viewport = str(eventArgs.viewport.camera)
-            editObj = str(app.activeEditObject)
-            document = str(app.activeDocument.name)
-            workspace = str(ui.activeWorkspace.name)
-
-            INFO = f'{_userId}|{_userName}|{_version}|{self.__class__}|{document}|{editObj}|w:{workspace}|{objectType}|{viewport}'
-            fusion_logger.info(INFO)
-
-        except:
-            ui = adsk.core.Application.get().userInterface
             if ui:
-                ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+                log.error(format(traceback.format_exc()))
+                ui.messageBox('Failed to start the Fusion Logger for Fusion 360 add-in:\n\n{}'.format(traceback.format_exc()))
+
+
+# Event handler for the commandStarting event.
+class CommandTerminatedHandler(adsk.core.ApplicationCommandEventHandler):
+    def __init__(self,):
+        super().__init__()
+    def notify(self, args):
+        ui = None
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+        try:
+            eventArgs = adsk.core.ApplicationCommandEventArgs.cast(args)
+            
+            
+            objectType = eventArgs.objectType
+            commandId = eventArgs.commandId
+            editObj = app.activeEditObject.objectType
+            document = app.activeDocument.name
+            workspace = ui.activeWorkspace.name
+            workspaceID = ui.activeWorkspace.id
+            className = str(self.__class__).split("_py.")
+            className = className[-1]
+            
+
+            if commandId:
+                INFO = f'{_userId}|{_userName}|{_version}|{className}|{document}|{editObj}|w:{workspace}|{workspaceID}|{objectType}|CommandTerminating|{commandId}'
+                log.info(INFO)
+
+            log.debug('Command terminated: {}'.format(commandId))
+        except:
+            if ui:
+                log.error(format(traceback.format_exc()))
+                ui.messageBox('Failed to start the Fusion Logger for Fusion 360 add-in:\n\n{}'.format(traceback.format_exc()))
+
+
+class CommandFusionLoggerAddIn:
+
+    def __init__(self):
+
+        ui = adsk.core.Application.get().userInterface
+        try:
+            # self.onHTMLEvent = OnHTMLEventHandler()
+            self.onClosed = MyCloseEventHandler()
+            
+            self.showFusionLoggerCmdDef = ui.commandDefinitions.itemById(COMMAND_ID)
+            if not self.showFusionLoggerCmdDef:
+                log.debug('Create Show Fusion Logger memu item.')
+                self.showFusionLoggerCmdDef = ui.commandDefinitions.addButtonDefinition(COMMAND_ID, 'Start Logger', 'Start Recording actions in Fusion 360', './resources/icon')
+
+            # Add the command to the toolbar.
+            panel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+            if panel:
+                cntrl = panel.controls.itemById(COMMAND_ID)
+                if not cntrl:
+                    log.debug('Add Show Fusion Logger to Add-Ins toolbar under Solids.')
+                    cmd = panel.controls.addCommand(self.showFusionLoggerCmdDef)
+                    cmd.isPromoted = True
+                    cmd.isPromotedByDefault = True
+
+            panel = ui.allToolbarPanels.itemById('CAMScriptsAddinsPanel')
+            if panel:
+                cntrl = panel.controls.itemById(COMMAND_ID)
+                if not cntrl:
+                    log.debug('Add Show Fusion Logger to Add-Ins toolbar under CAM.')
+                    cmd = panel.controls.addCommand(self.showFusionLoggerCmdDef)
+                    cmd.isPromoted = True
+                    cmd.isPromotedByDefault = True
+        except:
+            if ui:
+                log.error(format(traceback.format_exc()))
+                ui.messageBox('Failed to start the Fusion Logger for Fusion 360 add-in:\n\n{}'.format(traceback.format_exc()))
+        try:
+            # Attach a Starting Handler to intercept any command
+            log.info('Create Commmand Starting handler.')
+            self.onCommandStarting = CommandStartingHandler()
+            bindEventHandler(ui.commandStarting, self.onCommandStarting)
+
+            log.info('Create Command Terminated handler.')
+            self.onCommandTerminated = CommandTerminatedHandler()
+            bindEventHandler(ui.commandTerminated, self.onCommandTerminated)
+
+            # This must be sent through Fusion instead of calling it directly, otherwise
+            # Fusion will crash on startup.
+            cmdDef = ui.commandDefinitions.itemById(COMMAND_ID)
+            if cmdDef:
+                cmdDef.execute()
+        except:
+            if ui:
+                log.error(format(traceback.format_exc()))
+                ui.messageBox('Failed to start the Fusion Logger for Fusion 360 add-in:\n\n{}'.format(traceback.format_exc()))
+
+    def __del__(self):
+
+        ui = adsk.core.Application.get().userInterface
+
+        # Delete command created
+        unbindEventHandler(self.showPaletteCmdDef.commandCreated, self.onCommandCreated)
+
+        # Delete the palette created by this add-in.
+        palette = ui.palettes.itemById(PALETTE_ID)
+        if palette:
+            palette.deleteMe()
+        
+        # Delete controls and associated command definitions created by this add-ins
+        panel = ui.allToolbarPanels.itemById('SolidScriptsAddinsPanel')
+        cmd = panel.controls.itemById(COMMAND_ID)
+        if cmd:
+            cmd.deleteMe()
+
+        panel = ui.allToolbarPanels.itemById('CAMScriptsAddinsPanel')
+        if panel:
+            cmd = panel.controls.itemById(COMMAND_ID)
+            if cmd:
+                cmd.deleteMe()
+
+        if self.showPaletteCmdDef:
+            self.showPaletteCmdDef.deleteMe()
+
+        unbindEventHandler(ui.commandStarting, self.onCommandStarting)
+        unbindEventHandler(ui.commandTerminated, self.onCommandTerminated)
 
 
 def run(context):
+    global  _app, _ui, handlers, log, logger_handler, LOG_FILE_NAME, _userId, _userName, _version , _addin, _stopButton
+
     ui = None
+    ui = adsk.core.Application.get().userInterface
+
     try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        INFO = f'{_userId}|{_userName}|{_version}|session start'
-        fusion_logger.info(INFO)
-        ui.messageBox('Fusion Logger Started')
+        log = logging.getLogger(__name__)
+        logger_handler = logging.FileHandler(LOG_FILE_NAME)
+        logger_handler.setLevel(logging.INFO)
+        logger_formatter = logging.Formatter('%(asctime)s %(message)s')
+        logger_handler.setFormatter(logger_formatter)
+        log.addHandler(logger_handler)
+        log.setLevel(logging.INFO)
+        log.warn('Started Fusion Logger add-in.')
+        _app = adsk.core.Application.get()
+        _ui = _app.userInterface
+
+        _userId = _app.userId
+        _userName = _app.userName
+        _version = _app.version   
+
+        _addin = CommandFusionLoggerAddIn()
+        #_startButton = 
+        #_stopButton = CommandStopFusionLogger()
+
+        
 
     except:
         if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
+            log.error(format(traceback.format_exc()))
+            ui.messageBox('Failed to start the Fusion Logger for Fusion 360 add-in:\n\n{}'.format(traceback.format_exc()))
+
 
 def stop(context):
+    global _app, _ui, handlers, log, logger_handler, LOG_FILE_NAME, _userId, _userName, _version , _addin, _stopButton
     ui = None
+    
     try:
-        app = adsk.core.Application.get()
-        ui  = app.userInterface
-        # clearLoggingHandlers()
-        INFO = f'{_userId}|{_userName}|{_version}|session end'
-        fusion_logger.info(INFO)
-        ui.messageBox('Fusion Logger Stopped')
+        log.warn('Stopped Fusion Logger add-in.')
+        log.warn('------------------------------------------------------------')
+        log.removeHandler(logger_handler)
+        del log
+        # os.remove(LOG_FILE_NAME)
+        ui = adsk.core.Application.get().userInterface  
+        del _addin
+        del _stopButton
 
     except:
-        if ui:
-            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
-
-
-# setup handlers to trigger loggers
-try:
-    # "userInterface_var" is a variable referencing a UserInterface object.
-    onCommandStarting = MyCommandStartingHandler()
-    ui.commandStarting.add(onCommandStarting)
-    handlers.append(onCommandStarting)
-
-    # onCameraChanged = MyCameraChangedHandler()
-    # camera.add(onCameraChanged)
-    # handlers.append(onCameraChanged)
-
-
-except:
-    ui.messageBox('Handlers not set: \n{}'.format(traceback.format_exc())) 
+        pass
